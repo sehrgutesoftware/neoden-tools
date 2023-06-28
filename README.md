7ab6a86cef840593a7a537ea6b1c5f4d23ab1b0c
Joseph Paul <mail@jsph.pl>
2023-06-28 20:54:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NeoDen Tools
+
+- `kicad2nd4.py` converts KiCad position files (CSV) to NeoDen4 format
---
+++ kicad2nd4.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+"""
+Convert a KiCad position file (CSV) to NeoDen4 CSV format
+"""
+
+import argparse
+import csv
+import re
+from io import TextIOWrapper
+from typing import Callable
+
+PASSIVES_REGEX = re.compile(r'\w_(?P<size>\d+)_\d+Metric')
+
+
+def trim_package(input: str) -> str:
+    match = PASSIVES_REGEX.match(input)
+    if match is not None:
+        return match.group("size")
+    return input
+
+
+def format_position(input: str) -> str:
+    position = float(input)
+    return '{:.2f}mm'.format(position)
+
+
+def map_layer(input: str) -> str:
+    if input == "top":
+        return "T"
+    if input == "bottom":
+        return "B"
+    return ""
+
+
+def format_rotation(input: str) -> str:
+    angle = float(input)
+    if angle > 180:
+        angle = -1 * (360-angle)
+    return '{:.1f}'.format(angle)
+
+
+"""
+This dictionary maps NeoDen CSV fields to their corresponding
+KiCad fields along with an optional transformation function.
+"""
+FIELDS_MAP: dict[str, tuple[str, Callable[[str], str] | None]] = {
+    "Designator": ("Ref", None),
+    "Footprint": ("Package", trim_package),
+    "Mid X": ("PosX", format_position),
+    "Mid Y": ("PosY", format_position),
+    "Layer": ("Side", map_layer),
+    "Rotation": ("Rot", format_rotation),
+    "Comment": ("Val", None),
+}
+
+
+def transform_row(input: dict[str, str]) -> dict[str, str]:
+    output: dict[str, str] = {}
+
+    for neoden_field, (kicad_field, transform_function) in FIELDS_MAP.items():
+        value: str = input[kicad_field]
+        if transform_function is not None:
+            value = transform_function(value)
+        output[neoden_field] = value
+
+    return output
+
+
+def main(input: TextIOWrapper, output: TextIOWrapper) -> int:
+    neoden_fields = FIELDS_MAP.keys()
+    kicad_fields = [i[0] for i in FIELDS_MAP.values()]
+
+    reader = csv.DictReader(input)
+
+    writer = csv.DictWriter(output, neoden_fields)
+    writer.writeheader()
+    # "empty" line with just commas
+    writer.writerow({key: "" for key in neoden_fields})
+
+    for row in reader:
+        writer.writerow(transform_row(row))
+
+    input.close()
+    output.close()
+
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("input", type=argparse.FileType(
+        "r"), help="Input file (KiCad CSV position file)")
+    parser.add_argument("output", type=argparse.FileType(
+        "w"), help="Output filename")
+    args = parser.parse_args()
+    exit(main(args.input, args.output))
